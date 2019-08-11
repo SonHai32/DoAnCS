4ed19b6b54034467ce784bdff71dfb20093de9bc
root <root@localhost.localdomain>
2019-08-10 20:10:41 -0400

    add action setPrivateChannel

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -30,6 +30,7 @@ const user_reducer = (state = initialUserState, action) => {
 
 const initialChannelState = {
     currentChannel: null,
+    isPrivateChannel: false,
 }
 
 
@@ -39,6 +40,11 @@ const channel_reducer = (state = initialChannelState, action) => {
             return {
                 ...state,
                 currentChannel: action.payload.currentChannel
+            }
+        case actionTypes.SET_PRIVATE_CHANNEL:
+            return {
+                ...state,
+                isPrivateChannel: action.payload.isPrivateChannel
             }
         default:
             return state;

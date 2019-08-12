e928bfda24cad6251e850bafab2360592b54e804
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-08-11 23:21:06 -0400

    create metaPanel

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,11 +1,72 @@
 import React from 'react'
+import {Segment, Accordion, Header, Icon} from 'semantic-ui-react'
 
 class MetaPanel extends React.Component{
+
+    state = {
+        activeIndex: 0,
+        privateChannel: this.props.isPrivateChannel,
+
+        
+    }
+
+    setActiveIndex = (event, titleProps) =>{
+        const {index} = titleProps;
+        const {activeIndex} = this.state;
+        const newIndex = activeIndex === index ? -1 : index;
+        this.setState({activeIndex: newIndex});
+    }
+
     render(){
+
+        const {activeIndex, privateChannel} = this.state;
+
+        if(privateChannel) return null;
+
         return(
-            <div>
-                Mata Panel
-            </div>
+            <Segment>
+                <Header as="h3" attached="top">
+                    About # channel
+                </Header>
+                <Accordion styled attached="true">
+                    <Accordion.Title
+                        active={activeIndex ===0}
+                        index={0}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name='dropdown' />
+                        <Icon name="info" />
+                        Channel Detail
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex ===0}>
+                        Channel Detail
+                    </Accordion.Content>    
+                    <Accordion.Title
+                        active={activeIndex ===1}
+                        index={1}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name='dropdown' />
+                        <Icon name="user circle" />
+                        Channel Detail
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex ===1}>
+                         Detail
+                    </Accordion.Content>    
+                    <Accordion.Title
+                        active={activeIndex ===2}
+                        index={2}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name='dropdown' />
+                        <Icon name="pencil alternate" />
+                        Create By
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex ===2}>
+                        Detais
+                    </Accordion.Content>    
+                </Accordion>
+            </Segment>
         )
     }
 }

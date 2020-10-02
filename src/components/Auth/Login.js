a347484fa363a2ec2313d898ccce29800ab1af04
sonhai32 <lamsonhai1379@gmail.com>
2020-10-02 12:49:59 +0700

    update login and register components, add login with FB and GG

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Grid, Form, Segment, Button, Header, Message} from 'semantic-ui-react'
+import { Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 import firebase from '../../firebase'
 
+import background from '../../Asset/background/loginBG.png'
+
+import fbSignIn from "../Auth/FacebookAuth"
+import ggSignIn from "../Auth/GoogleAuth"
 
 
 class Login extends React.Component {
@@ -28,7 +32,6 @@ class Login extends React.Component {
             .auth()
             .signInWithEmailAndPassword(this.state.email, this.state.password)
             .then(signinUser =>{
-            console.log('Sign in'); 
             this.setState({loading: false});
         })
         .catch(err =>{
@@ -36,11 +39,19 @@ class Login extends React.Component {
         })
         }
         else{
-            let error = {message: 'Please fill all fields'};
+            let error = {message: 'Vui lòng nhập đủ thông tin'};
             let  errors = [];
             this.setState({errors: errors.concat(error), loading:false})
         }
     
+    }
+    handleFBLogin = event =>{
+        event.preventDefault();
+        fbSignIn();
+    }
+    handleGGLogin = event =>{
+        event.preventDefault();
+        ggSignIn();
     }
     isFormValid = ({email, password}) => email && password;
 
@@ -48,31 +59,58 @@ class Login extends React.Component {
 
     render() {
         const {email, password, errors, loading} = this.state;
+
+        const myStyle ={
+            mainContainer:{
+                backgroundImage: `url('${background}')`,
+                backgroundPosition: 'right',
+                backgroundSize: 'cover',
+                height: '100vh'
+            }
+        }
         return (
-            <Grid textAlign="center" verticalAlign="middle" className="app">
+            <Grid textAlign="center"  verticalAlign="middle" stretched  className="app" style={myStyle.mainContainer}>
                 <Grid.Column style={{maxWidth: 450}}>
-                    <Header as="h2" icon color="violet" textAlign="center">
+                    <Header as="h1" icon color="purple" textAlign="center">
+                        <Icon name='paw' color='purple'>
+
+                        </Icon>
                         Đăng Nhập
                     </Header>
-                    <Form size="large" onSubmit={this.handleSubmit}>
-                        <Segment stacked>
+                        <Segment >
+                    <Form size="large" onSubmit={this.handleSubmit} >
                             <Form.Input fluid name="email" icon="mail" iconPosition="left"
                             placeholder="Địa chỉ email" type="text" onChange={this.handleChange} value={email} />
                             <Form.Input fluid name="password" icon="key" iconPosition="left"
                             placeholder="Password" type="password" onChange={this.handleChange} value={password} />
-                            <Button disabled={loading} className={loading ? 'loading' : ''} fluid size="large" color="violet">Login</Button>
-                            {errors.length > 0 &&(
+                            <Button disabled={loading} className={loading ? 'loading' : ''} fluid size="large" color="purple">Login</Button>
+                            
+                            <Message>
+                                 Bạn chưa có tài khoản đăng ký ngay ? <Link to="/register"> Đăng Ký</Link>
+                            </Message>
+                <Message >
+                    <p>Đăng nhập bằng</p>
+
+                <Button fluid color='facebook' onClick={this.handleFBLogin}>
+                    <Icon name='facebook'></Icon>
+                    Facebook
+                    </Button> 
+                <br></br>
+                <Button  fluid color='green' onClick={this.handleGGLogin}>
+                    <Icon name='google'></Icon>
+                        Google
+                    </Button>
+                 
+                 </Message>
+{errors.length > 0 &&(
                                 <Message color="red">
                                     <h3>Error</h3>
                                     {this.displayError(errors)}
                                 </Message>
                             )}
-                            <Message>
-                                 Bạn chưa có tài khoản đăng ký ngay ? <Link to="/register"> Đăng Ký</Link>
-                            </Message>
-                        </Segment>
 
                     </Form>
+                        </Segment>
                 </Grid.Column>
             </Grid>
         );
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,215 +1,247 @@
 import React from "react";
 import {
-  Grid,
-  Form,
-  Segment,
-  Button,
-  Header,
-  Message,
-  Icon,
+    Grid,
+    Form,
+    Segment,
+    Button,
+    Header,
+    Message,
+    Icon,
 } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import firebase from "../../firebase";
 import md5 from "md5";
+
+import background from '../../Asset/background/loginBG.png'
+
+
+
+
 class Register extends React.Component {
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    passwordConfirmation: "",
-    errors: [],
-    loading: false,
-    usersRef: firebase.database().ref("users"),
-  };
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+        errors: [],
+        loading: false,
+        usersRef: firebase.database().ref("users"),
+    };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (this.isFormValid()) {
-      this.setState({ errors: [], loading: true });
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(this.state.email, this.state.password)
-        .then((createdUser) => {
-          console.log(createdUser);
-          var emailHashToMD5 = md5(createdUser.user.email);
-          createdUser.user
-            .updateProfile({
-              displayName: this.state.username,
-              photoURL:
-                "https://www.gravatar.com/avatar/" +
-                emailHashToMD5 +
-                "?d=identicon",
-            })
-            .then(() => {
-              this.saveUser(createdUser).then(() => {
-                console.log("User Saved");
-                this.setState({ loading: false });
-              });
-            })
-            .catch((err) => {
-              console.log(err);
-              this.setState({
-                errors: this.state.errors.concat(err),
-                loading: false,
-              });
-            });
-        })
-        .catch((err) => {
-          console.error(err);
-          this.setState({
-            errors: this.state.errors.concat(err),
-            loading: false,
-          });
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
         });
-    }
-  };
+    };
 
-  handleInputError = (errors, inputName) => {
-    return errors.some((error) =>
-      error.message.toLowerCase().includes(inputName)
-    )
-      ? "error"
-      : "";
-  };
+    handleRegisterSubmit = (event) => {
+        /// preventDefault() của đối tượng event được sử dụng để ngăn chặn xử lý mặc định của trình duyệt khi xảy ra sự kiện.
+        event.preventDefault();
 
-  isFormValid = () => {
-    let error;
-    let errors = [];
-    if (this.isFormEmpty(this.state)) {
-      error = { message: "Please fill all the fields" };
-      this.setState({ errors: errors.concat(error) });
-      return false;
-    } else if (!this.passwordValid(this.state)) {
-      error = { message: "Password is invalid" };
-      this.setState({ errors: errors.concat(error) });
-      return false;
-    } else {
-      this.setState({ errors: [] });
-      return true;
-    }
-  };
 
-  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
-    return (
-      !username.length ||
-      !email.length ||
-      !password.length ||
-      !passwordConfirmation.length
-    );
-  };
-  // isEmailValid = ({email}) => {
-  //     let emailPattern = '^[a-z][a-z0-9_\\.]{5,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$'
-  //     return email.match(emailPattern);
-  // }
-  passwordValid = ({ password, passwordConfirmation }) => {
-    if (password.length < 6 || passwordConfirmation.length < 6) {
-      return false;
-    } else if (password !== passwordConfirmation) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-  saveUser = (createdUser) => {
-    return this.state.usersRef.child(createdUser.user.uid).set({
-      name: createdUser.user.displayName,
-      avatar: createdUser.user.photoURL,
-      email: createdUser.user.email
-    });
-  };
-  displayError = (errors) => errors.map((i) => <p key={i}> {i.message} </p>);
+        if (this.isFormValid()) {
+            /// set loading to true => display loading wheel
+            this.setState({ errors: [], loading: true });
+            /// register with email and password in firebase
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.state.email, this.state.password)
+                .then((createdUser) => {
 
-  render() {
-    const {
-      username,
-      email,
-      password,
-      passwordConfirmation,
-      errors,
-      loading,
-    } = this.state;
-    return (
-      <Grid textAlign="center" verticalAlign="middle" className="app">
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="orange" textAlign="center">
-            <Icon name="puzzle piece" color="orange" />
+                    var emailHashToMD5 = md5(createdUser.user.email);
+                    createdUser.user
+                        .updateProfile({
+                            displayName: this.state.username,
+                            photoURL:
+                                "https://www.gravatar.com/avatar/" +
+                                emailHashToMD5 +
+                                "?d=identicon",
+                        })
+                        .then(() => {
+                            this.saveUser(createdUser).then(() => {
+                                this.setState({ loading: false });
+                            });
+                        })
+                        .catch((err) => {
+                            this.setState({
+                                errors: this.state.errors.concat(err),
+                                loading: false,
+                            });
+                        });
+                })
+                .catch((err) => {
+                    console.error(err);
+                    this.setState({
+                        errors: this.state.errors.concat(err),
+                        loading: false,
+                    });
+                });
+        }
+    };
+
+    
+
+    handleInputError = (errors, inputName) => {
+        return errors.some((error) =>
+            error.message.toLowerCase().includes(inputName)
+        )
+            ? "error"
+            : "";
+    };
+
+    ///
+    /// check everything is vaild
+    ///
+    isFormValid = () => {
+        let error;
+        let errors = [];
+        if (this.isFormEmpty(this.state)) {
+            error = { message: "Please fill all the fields" };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else if (!this.passwordValid(this.state)) {
+            error = { message: "Password is invalid" };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else {
+            this.setState({ errors: [] });
+            return true;
+        }
+    };
+    
+    ///
+    /// check empty input
+    ///
+
+    isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return (
+            !username.length ||
+            !email.length ||
+            !password.length ||
+            !passwordConfirmation.length
+        );
+    };
+    
+    ///
+    ///check valid Password
+    ///
+    passwordValid = ({ password, passwordConfirmation }) => {
+        if (password.length < 6 || passwordConfirmation.length < 6) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+    ///
+    ///Save user to database-firebase if success full register
+    ///
+    saveUser = (createdUser) => {
+        return this.state.usersRef.child(createdUser.user.uid).set({
+            name: createdUser.user.displayName,
+            avatar: createdUser.user.photoURL,
+            email: createdUser.user.email
+        });
+    };
+    displayError = (errors) => errors.map((i) => <p key={i}> {i.message} </p>);
+
+    render() {
+        const {
+            username,
+            email,
+            password,
+            passwordConfirmation,
+            errors,
+            loading,
+        } = this.state;
+        const myStyle ={
+            mainContainer:{
+                backgroundImage: `url('${background}')`,
+                backgroundPosition: 'right',
+                backgroundSize: 'cover',
+                height: '100vh'
+            }
+        }
+        return (
+            <Grid textAlign="center" style={{height: '100vh'}}  verticalAlign="middle" className="app"  >
+                {/* <Grid.Row style={{backgroundImage: `url('${background}')`}}> */}
+                <Grid.Row style={myStyle.mainContainer} >
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as="h2" icon color="purple" textAlign="center">
+                        <Icon name="paw" color="purple" />
             Đăng Ký {" "}
-          </Header>{" "}
-          <Form size="large" onSubmit={this.handleSubmit}>
-            <Segment stacked>
-              <Form.Input
-                fluid
-                name="username"
-                icon="user"
-                iconPosition="left"
-                placeholder="Tên tài khoản"
-                type="text"
-                onChange={this.handleChange}
-                value={username}
-              />
-              <Form.Input
-                fluid
-                name="email"
-                icon="mail"
-                iconPosition="left"
-                placeholder="Địa chỉ email"
-                type="text"
-                onChange={this.handleChange}
-                value={email}
-                className={this.handleInputError(errors, "email")}
-              />
-              <Form.Input
-                fluid
-                name="password"
-                icon="lock"
-                iconPosition="left"
-                placeholder="Mật khẩu"
-                type="password"
-                onChange={this.handleChange}
-                value={password}
-                className={this.handleInputError(errors, "password")}
-              />
-              <Form.Input
-                name="passwordConfirmation"
-                icon="repeat"
-                iconPosition="left"
-                placeholder="Nhập lại mật khẩu"
-                type="password"
-                onChange={this.handleChange}
-                value={passwordConfirmation}
-                className={this.handleInputError(errors, "password")}
-              />
-              <Button
-                disabled={loading}
-                className={loading ? "Đợi xíu nhé  ..." : ""}
-                color="orange"
-                fluid
-                size="large"
-              >
-                {" "}
-                Submit{" "}
+                    </Header>{" "}
+                    <Form size="large" onSubmit={this.handleRegisterSubmit}>
+                        <Segment stacked>
+                            <Form.Input
+                                fluid
+                                name="username"
+                                icon="user"
+                                iconPosition="left"
+                                placeholder="Tên tài khoản"
+                                type="text"
+                                onChange={this.handleChange}
+                                value={username}
+                            />
+                            <Form.Input
+                                fluid
+                                name="email"
+                                icon="mail"
+                                iconPosition="left"
+                                placeholder="Địa chỉ email"
+                                type="text"
+                                onChange={this.handleChange}
+                                value={email}
+                                className={this.handleInputError(errors, "email")}
+                            />
+                            <Form.Input
+                                fluid
+                                name="password"
+                                icon="lock"
+                                iconPosition="left"
+                                placeholder="Mật khẩu"
+                                type="password"
+                                onChange={this.handleChange}
+                                value={password}
+                                className={this.handleInputError(errors, "password")}
+                            />
+                            <Form.Input
+                                name="passwordConfirmation"
+                                icon="repeat"
+                                iconPosition="left"
+                                placeholder="Nhập lại mật khẩu"
+                                type="password"
+                                onChange={this.handleChange}
+                                value={passwordConfirmation}
+                                className={this.handleInputError(errors, "password")}
+                            />
+                            <Button
+                                disabled={loading}
+                                className={loading ? "Đợi xíu nhé  ..." : ""}
+                                color="purple"
+                                fluid
+                                size="large"
+                            >
+                                Đăng ký
               </Button>{" "}
-              {errors.length > 0 && (
-                <Message color="red">
-                  <h3> Error </h3> {this.displayError(errors)}{" "}
-                </Message>
-              )}
-              <Message>
-               Bạn đã có tài khoản ? <Link to="/Login"> Đăng nhập </Link>{" "}
-              </Message>{" "}
-            </Segment>{" "}
-          </Form>{" "}
-        </Grid.Column>{" "}
-      </Grid>
-    );
-  }
+<Message>
+                                Bạn đã có tài khoản ? <Link to="/Login"> Đăng nhập </Link>{" "}
+                            </Message>{" "}
+                 
+                            {errors.length > 0 && (
+                                <Message color="red">
+                                    <h3> Error </h3> {this.displayError(errors)}{" "}
+                                </Message>
+                            )}
+                            
+                        </Segment>{" "}
+                    </Form>{" "}
+                </Grid.Column>{" "}
+</Grid.Row>
+            </Grid>
+        );
+    }
 }
 
 export default Register;
